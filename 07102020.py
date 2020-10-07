c8768d22a04f7bdd342ac8a596380f4371db13a1
Ilie Stolnicu <stolnicu@gmail.com>
2020-10-07 21:30:19 +0300

    mesaj

--- 07102020.py
+++ 07102020.py
@@ -11,7 +11,7 @@ print('Ana are mere \nIon are pere.')
 print("""
 \tAna are mere
 Petre are cuie\n\n""") #3 ghilimele = \n; \t = TAB
-variabila1 = 1
+variabila1 = 1 #variabilele se denumesc cu primul caracter de tip litera, fara spatii, cu underscore _
 variabila2 = 2
 variabila3 = f"Ana are {variabila1} mar si {variabila2} pere."
 print(variabila3)
@@ -19,3 +19,17 @@ variabila4 = "Ana are {1} mar si {0} pere".format(variabila1, variabila2)
 print(variabila4)
 variabila5 = "Ana are {1} mar si {1} pere".format(variabila1, variabila2)
 print(variabila5)
+print("variabila4 =>>", type(variabila4))
+print("variabila2 =>>", type(variabila2))
+variabila6 = ("Ana are " + str(variabila2) + " mere.") #str() transformare in string
+print(variabila6)
+print(variabila1 + variabila2)
+print(str(variabila1) + str(variabila2))
+print(variabila2 - variabila1)
+print(variabila1 / variabila2)
+variable_number_1 = 3 - 2j
+print(variable_number_1.real)
+print(variable_number_1.imag)
+print(variable_number_1.conjugate())
+
+
---
+++ 07102020instructiuni.py
@@ -0,0 +1,28 @@
+#if conditie:
+#    sintaxa1
+#elif conditie2:
+#    sintaxa2
+#else:
+#    sintaxa3
+a = 1
+b = 2
+c = 3
+#if a > 4:
+#    print(a)
+#elif a > b:
+#    print(b)
+#elif c < a:
+#    print(c)
+#else:
+#    print("Niciun rezultat gasit")
+d = None
+if a < b:
+    d = a + b
+    print(d)
+else:
+    print(d)
+
+if a ==1
+    print(a)
+else:
+    a = 2
