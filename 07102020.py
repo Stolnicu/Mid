a987d463c8018482b414c23caba666d5caf2ce41
Ilie Stolnicu <stolnicu@gmail.com>
2020-10-07 20:42:10 +0300

    mesaj

--- 07102020.py
+++ 07102020.py
@@ -5,5 +5,17 @@ print('doi')
 # print(a)
 print("Elevul 'X' nu si-a facut tema")
 print('Elevul "X" nu si-a facut tema') #folosim tipuri de ghilimele diferite pentru start/end si interior
-a = input("Mesaj")
-Print('double check')
+#a = input("Mesaj")
+print('double check')
+print('Ana are mere \nIon are pere.')
+print("""
+\tAna are mere
+Petre are cuie\n\n""") #3 ghilimele = \n; \t = TAB
+variabila1 = 1
+variabila2 = 2
+variabila3 = f"Ana are {variabila1} mar si {variabila2} pere."
+print(variabila3)
+variabila4 = "Ana are {1} mar si {0} pere".format(variabila1, variabila2)
+print(variabila4)
+variabila5 = "Ana are {1} mar si {1} pere".format(variabila1, variabila2)
+print(variabila5)
