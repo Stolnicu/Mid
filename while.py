f91d2291c09d133ab59113d5f29b2a2a05ac89cd
Ilie Stolnicu <68511664+Stolnicu@users.noreply.github.com>
2020-10-12 21:51:43 +0300

    12.10.2020

---
+++ while.py
@@ -0,0 +1,22 @@
+# while conditionare:
+#     atribuire1 / instructiune1
+#     atribuire2 / instructiune2
+#
+#     atribuiren / instructiunen
+#
+# while True:
+#     print('ruleaza')
+#     break
+
+# cel_mai_mare_numar = 999999
+# number = int(input('Introdu un numar'))
+# while number != -1:
+#     if number > cel_mai_mare_numar or (cel_mai_mare_numar > number > -1):
+#         number -= 1
+#     elif number < -1:
+#         number += 1
+#     else:
+#         print(cel_mai_mare_numar)
+#     print(number)
+
+numbers = int(input("Introdu un sir de numere: "))
