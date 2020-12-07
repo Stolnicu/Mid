6bc27d0f61442995a33949405c3621faf0e37f10
Ilie Stolnicu <68511664+Stolnicu@users.noreply.github.com>
2020-12-07 21:49:46 +0200

    Update Mid.py

--- Mid.py
+++ Mid.py
@@ -8,20 +8,24 @@ string = "The Inquisitor must meet Varric on top of Skyhold's battlements to be 
 
 patches = [[5, 14, "Conquistador"], [26, 31, "King"], [43, 49, "Palace"]]
 
-def inlocuire(string, newstring, index, nofail=False):
+def inlocuire(oldstring, newstring, start_index, end_index, nofail=False):
     # raise an error if index is outside of the string
-    if not nofail and index not in range(len(string)):
+    if not nofail and start_index and end_index not in range(len(oldstring)):
         raise ValueError("index outside given string")
 
     # if not error, but the index is still not in the correct range
-    if index < 0:  # add it to the beginning
-        return newstring + string
+    if start_index < 0:  # add it to the beginning
+        return newstring + oldstring
 
-    if index > len(string):  # add it to the end
-        return string + newstring
+    if end_index > len(oldstring):  # add it to the end
+        return oldstring + newstring
 
     # insert the new string between "slices" of the original
-    return string[:start_index] + newstring + string[end_index:]
+    return oldstring[:start_index] + newstring + oldstring[end_index:]
+
+
+inlocuire(string(), "King", start_index=input('Indexul de pornire: '), end_index=input('Indexul de sfarsit: '))
+
  
 
 # Problema 2
